5292c0c788f32aa701fa75fd27d559add146e426
duckdzung <laiducdung1911@gmail.com>
2024-02-16 16:58:07 +0700

    init

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+    // Độ dài của mỗi câu có thể tùy ý, bạn có thể viết mười câu, hai mươi câu đều được
+    // Hãy cố gắng để mỗi câu không quá 15 từ, nếu không có thể ảnh hưởng đến hiệu ứng hiển thị
+    texts: [
+        "Gửi Thu Hà",      //Ở đây, dấu phẩy cuối cùng của mỗi câu phải là tiếng Anh nhé!! Rất quan trọng đấy!!
+        "Người yêu của tui",  // Tương tự như trên...
+        "Hôm nay là sinh nhật của em",
+        "Chúc em tuổi mới xinh đẹp, cười nhiều và hạnh phúcc. Luôn gặp nhiều may mắn và vững vàng hơn nhaa.",
+        "Hẹn chờ lên SG rồi đi Phước Hải nè.",
+        "See u again",
+    ],
+    imgs: {
+        "thuha": "./imgs/thuha.jpg",
+    },
+    // Mô tả văn bản của các nút, dưới đây là văn bản mặc định của nút, bằng tiếng Anh, bạn có thể thay đổi thành văn bản bạn thích
+    desc: {
+        turn_on: "Dô nè",
+        play: "Mở nhạc nhá",
+        bannar_coming: "Ấn tiếp nhá",
+        balloons_flying: "Bóng bay nè",
+        cake_fadein: "Bánh kem đâu ta",
+        light_candle: "Đốt lửa nè",
+        wish_message: "Háp pi bớt đây tu diuuuu",
+        story: "Lời nhắn dành cho Thu Hà",
+    }
+};
